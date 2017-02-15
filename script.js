c7fca9a54cd0ff713dd13a066457778b945df032
Unknown <jessithejinx@gmail.com>
2017-02-15 13:53:56 +0000

    Comment updates
    Comments have been included

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $( document ).ready(function(){
-
+/*this is necessary as the browser will try to execute the below function 
+before the document is ready and so it does not run without this*/
     $( "#submit" ).on( "click", function() {
         console.log( "click" );
     });
