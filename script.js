a0d3175146fb2a467752e304b598ce5b06647387
Jessica Rocton <jessithejinx@gmail.com>
2017-03-08 14:41:09 +0000

    Now uploads and prints a file to console

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 $( document ).ready(function(){
 /*this is necessary as the browser will try to execute the below function 
-before the document is ready and so it does not run without this*/
+before the document is ready and so it does not run reliably without this*/
+	var myData = [];
     $( "#submit" ).on( "click", function() {
-        console.log( "click" );
-    });
+          var fileInput = document.getElementById('upload');
+			var file = fileInput.files[0];
+			var textType = /text.*/;
 
+			if (file.type.match(textType)) {
+				var reader = new FileReader();
+
+				reader.onload = function() { //main bulk of function 
+					//console.log (reader.result);
+					myData = reader.result.split(' ');
+					console.log (myData);
+					
+				}
+
+				reader.readAsText(file);	
+			} else {
+				console.log ("File not supported!");
+			}
+    });
 });
